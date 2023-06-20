91853ae942dd059afeda0c97bc4ac739d9d9ed63
taufeeque-bagmaru <taufeeque.bagmaru@gmail.com>
2023-06-20 13:17:54 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
